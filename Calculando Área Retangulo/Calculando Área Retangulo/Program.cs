d5adfd959d8834bd73606f36a72c49a2aad34d56
perkles <ismthiago@gmail.com>
2021-10-17 23:16:47 -0300

    Incluindo Exercício Cálculo de Área Retangulo e enunciados

---
+++ Calculando Área Retangulo/Calculando Área Retangulo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculando_Área_Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo_Quadrado interacao = new Retangulo_Quadrado();//INSTANCIANDO O OBJETO INTERAÇÃO
+            
+            Console.WriteLine("PROGRAMA PARA CALCULAR ÁREA DO RETANGULO");
+            Console.Write("DIGITE O VALOR DO LADO DO RETANGULO: ");
+            interacao.Lado = float.Parse(Console.ReadLine());//RECEBENDO O LADO DO RETANGULO
+            while(interacao.Lado <= 0)//VALIDANDO O VALOR RECEBIDO. OBS: ESTA VALIDAÇÃO PODERIA TER SIDO FEITO NO MÉTODO DE ACESSO, MAS AINDA NÃO SEI.
+            {
+                Console.WriteLine("VALOR INVÁLIDO !");
+                Console.Write("DIGITE O VALOR DO LADO DO RETANGULO: ");
+                interacao.Lado = float.Parse(Console.ReadLine());
+            }
+            
+            
+            Console.Write("DIGITE O VALOR DA ALTURA DO RETANGULO: ");
+            interacao.Altura = float.Parse(Console.ReadLine());//RECEBENDO A ALTURA DO RETANGULO
+            while(interacao.Altura <= 0)//VALIDANDO
+            {
+                Console.WriteLine("VALOR INVÁLIDO !");
+                Console.Write("DIGITE O VALOR DA ALTURA DO RETANGULO: ");
+                interacao.Altura = float.Parse(Console.ReadLine());
+            }
+            
+            
+            interacao.ExibirDados();//CHAMANDO O MÉTODO "EXIBIRDADOS"
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Calculando Área Retangulo/Calculando Área Retangulo/Retangulo-Quadrado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculando_Área_Retangulo
+{
+    public class Retangulo_Quadrado//CLASSE CRIADA PARA RECEBER AS PROPRIEDADES DO RETANGULO
+    {
+        private float lado;//CRIANDO UM MÉTODO DE ACESSO PARA O VALOR "LADO" E ENCAPSULANDO A MESMA
+
+        public float Lado
+        {
+            get { return lado; }
+            set { lado = value; }
+        }
+
+        private float altura;//CRIANDO UM MÉTODO DE ACESSO PARA  O VALOR "ALTURA"E ENCAPSULANDO A MESMA
+
+        public float Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        //VOU CRIAR UM CONSTRUTOR QUE RECEBERÁ OS DOIS PARÂMETROS
+
+        public Retangulo_Quadrado(float lado = 0 , float altura = 0)
+        {
+            this.Lado = lado;
+            this.Altura = altura;
+        }
+
+        //CRIANDO MÉTODO PARA EXIBIR DADOS
+
+        public void ExibirDados()
+        {
+            Console.Write("A Área é: " + CalcularArea());
+        }
+
+        //CRIANDO UM MÉTODO PARA CALCULAR A ÁREA
+        private float CalcularArea()
+        {
+            return Lado * Altura;
+        }
+
+    }
+}
